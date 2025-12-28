f260fae856ea26ff9332b44cf87d3a2dd334f032
Saiteja <saiteja.infasta@gmail.com>
2025-12-28 12:23:50 +0530

    done setup

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello From hostinger");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
+
+export default app;
